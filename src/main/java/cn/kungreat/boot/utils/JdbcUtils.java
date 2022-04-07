2a3a9997deec92946ee872628143310b10a411a3
kungreat2021 <kungreat2021@outlook.com>
2022-04-07 16:05:18 +0800

    加入数据库连接池.引入mysql8.x. 不需要log4g 引用太多

---
+++ src/main/java/cn/kungreat/boot/utils/JdbcUtils.java
@@ -0,0 +1,40 @@
+package cn.kungreat.boot.utils;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JdbcUtils {
+
+    private final static HikariDataSource DATA_SOURCE;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setDataSourceClassName("");
+        config.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/socketcharts?useSSL=true&serverTimezone=GMT%2B8");
+        config.setUsername("root");
+        config.setPassword("yjssaje");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "50");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "512");
+        DATA_SOURCE = new HikariDataSource(config);
+    }
+
+/*    默认10个连接池
+    One Connection Cycle is defined as single DataSource.getConnection()/Connection.close().
+    One Statement Cycle is defined as single
+    Connection.prepareStatement(),
+    Statement.execute(), Statement.close()
+    */
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+            connection =  DATA_SOURCE.getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
+}
