2f1fb716c6ba4d30fcda6cd234e1fd7d7eff7664
kungreat2021 <kungreat2021@outlook.com>
2022-04-26 20:24:41 +0800

    主方法启动

--- src/main/test/cn/kungreat/test/test.java
+++ src/main/test/cn/kungreat/test/test.java
@@ -1,43 +1,11 @@
 package cn.kungreat.test;
 
-import cn.kungreat.boot.NioBossServerSocket;
-import cn.kungreat.boot.NioWorkServerSocket;
-import cn.kungreat.boot.handler.WebSocketChannelInHandler;
-import cn.kungreat.boot.handler.WebSocketChannelOutHandler;
-import cn.kungreat.boot.handler.WebSocketProtocolHandler;
-import cn.kungreat.boot.impl.ChooseWorkServerImpl;
-import cn.kungreat.boot.impl.DefaultLogServer;
-import cn.kungreat.boot.impl.NioBossServerSocketImpl;
-import cn.kungreat.boot.impl.NioWorkServerSocketImpl;
-
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.StandardSocketOptions;
-import java.util.logging.Logger;
+
 
 public class test {
     public static void main(String[] args) throws IOException {
-        NioBossServerSocket nioBossServerSocket = NioBossServerSocketImpl.create();
-        nioBossServerSocket.buildChannel();
-        nioBossServerSocket.buildThread();
-        Logger log = DefaultLogServer.createLog("D:\\kungreat\\IdeaProjects\\log2"
-                ,"kungreat.cn");
-        nioBossServerSocket.setLogger(log);
-        ChooseWorkServerImpl chooseWorkServer = new ChooseWorkServerImpl();
-        NioWorkServerSocketImpl.addChannelInHandlers(new WebSocketChannelInHandler());
-        NioWorkServerSocketImpl.addChannelOutHandlers(new WebSocketChannelOutHandler());
-        NioWorkServerSocketImpl.addChannelProtocolHandler(new WebSocketProtocolHandler());
-        NioWorkServerSocket[] workServerSockets = new NioWorkServerSocket[12];
-        for(int x=0;x<workServerSockets.length;x++){
-            NioWorkServerSocket workServerSocket = NioWorkServerSocketImpl.create();
-            workServerSocket.buildThread();
-            workServerSocket.buildSelector(8192);
-//            workServerSocket.setOption​(StandardSocketOptions.SO_KEEPALIVE,true);
-            workServerSockets[x]=workServerSocket;
-        }
-        nioBossServerSocket.start(new InetSocketAddress(InetAddress.getLocalHost(),9999),workServerSockets,chooseWorkServer);
-//        ByteBuffer byteBuffer = ByteBuffer.allocate(12);
+        //        ByteBuffer byteBuffer = ByteBuffer.allocate(12);
 //        byteBuffer.put("dfsdfsdfsfsf".getBytes());
 //        System.out.println(byteBuffer.capacity());
 //        System.out.println(byteBuffer.remaining());
