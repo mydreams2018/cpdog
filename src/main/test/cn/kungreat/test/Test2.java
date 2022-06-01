eb1e5628bb520c173ed0b8e9d8cee0d2f4510a52
kungreat2021 <kungreat2021@outlook.com>
2022-06-01 21:18:34 +0800

    加入日志-文件测试

--- src/main/test/cn/kungreat/test/Test2.java
+++ src/main/test/cn/kungreat/test/Test2.java
@@ -2,6 +2,10 @@ package cn.kungreat.test;
 
 import cn.kungreat.boot.utils.CutoverBytes;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
 import java.text.Collator;
 import java.util.Comparator;
 import java.util.Locale;
@@ -19,10 +23,19 @@ public class Test2 {
 //        if(bt<0){
 //            System.out.println("小0");
 //        }
-        byte[] bts = {0,-1,-1,-1};
-        System.out.println(CutoverBytes.readInt(bts));
-        //定义一个中文排序器
-        Comparator c = Collator.getInstance(Locale.CHINA);
+//        byte[] bts = {0,-1,-1,-1};
+//        System.out.println(CutoverBytes.readInt(bts));
+//        //定义一个中文排序器
+//        Comparator c = Collator.getInstance(Locale.CHINA);
+
+        FileReader fileReader = new FileReader(new File("D:\\kungreat\\IdeaProjects\\log1\\sys-info.log"), StandardCharsets.UTF_8);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        bufferedReader.lines().forEach(e ->{
+            if(e.contains("WebSocketChannelOutHandler")){
+                System.out.println(e.substring(e.length() - 3).trim());
+            }
+        });
+//        System.out.println(bufferedReader.lines().filter(e->e.contains("WebSocketChannelOutHandler")).count());
 
     }
 }
